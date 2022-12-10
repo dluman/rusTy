f950e8624c0d8a769109d6ed6788ed4f7df18375
Christopher May <christopher.may@astrazeneca.com>
2022-12-10 22:32:34 +0000

    clearer example

--- src/lib/phrasematcher.rs
+++ src/lib/phrasematcher.rs
@@ -13,14 +13,14 @@ pub fn match_phrases() {
     let spacy: Module = spacy::Module::init();
     spacy.load("en_core_web_sm");
 
-    let text: spacy::doc::Doc = spacy::nlp("Cheese is tasty.");
+    let text: spacy::doc::Doc = spacy::nlp("United Kingdom and United States");
 
     let builtins: PyModule = PyModule::import(python, "spacy.matcher").unwrap();
     let phrasematcher: PyObject = builtins.get(python, "PhraseMatcher").unwrap();
     let vocab: PyObject = spacy::vocab();
     let matcher: PyObject = phrasematcher.call(python, (vocab,), None).unwrap();
 
-    let phrases_to_match: PyList = vec!["Cheese"].to_py_object(python);
+    let phrases_to_match: PyList = vec!["United Kingdom", "United States"].to_py_object(python);
     let patterns: PyObject = spacy::tokenizer(phrases_to_match);
     matcher
         .call_method(python, "add", ("FOOD", patterns), None)
