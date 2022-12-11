2034a2c518820babb61c83a336f813ea2a75d691
Christopher May <christopher.may@astrazeneca.com>
2022-12-11 21:46:46 +0000

    include types

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use crate::lib::{phrasematcher, spacy};
+use crate::lib::{doc, phrasematcher, spacy};
 pub mod lib;
 // pub mod lib;
 
-fn main() {
-    let spacy = spacy::Module::init();
-    spacy.load("en_core_web_sm");
-    let pangram1 = spacy::nlp("This is good.");
-    let pangram2 = spacy::nlp("This is bad.");
+use cpython::*;
 
-    let sim = pangram1.similarity(pangram2);
+fn main() {
+    let spacy: spacy::Module = spacy::Module::init();
+
+    spacy.load("en_core_web_sm");
+    let pangram1: doc::Doc = spacy::nlp("This is good.");
+    let pangram2: doc::Doc = spacy::nlp("This is bad.");
+
+    let sim: PyObject = pangram1.similarity(pangram2);
     println!("{:?}", sim);
 
     phrasematcher::match_phrases();
